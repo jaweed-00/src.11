97fc9e92fe33edd580718e8682614a9f9d3f408f
jaweed-00 <jhamzehloui@yahoo.com>
2024-01-29 01:02:03 +0300

    exercise 1 completed

--- javid_tasks/ex1.php
+++ javid_tasks/ex1.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
       <title>Exercise 1: Getting Started with PHP - javid</title>
 </head>
-<body>
+<body> 
     <h3>3.1 Write PHP code to output the following message:
     Hello world! My name is "David"
     </h3>
