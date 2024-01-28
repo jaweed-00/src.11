1daedce76f32bbbd2b81551ab09105926b614a8f
jaweed-00 <jhamzehloui@yahoo.com>
2024-01-29 00:51:14 +0300

    exercise 1 completed

---
+++ hello.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>php tesk</title>
+</head>
+<body>
+<?php
+//this is a hello comment
+echo "<h1>hello world</h1>";
+
+?>
+    
+</body>
+</html>
+
+
