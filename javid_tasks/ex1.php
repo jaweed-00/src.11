eb448bc86dbed8e7f6f63c37fcd62cfdee6fca40
jaweed-00 <jhamzehloui@yahoo.com>
2024-01-29 00:56:55 +0300

    excercise 1 completed

--- javid_tasks/ex1.php
+++ javid_tasks/ex1.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Exercise 1: Getting Started with PHP - javid</title>
+     <title>Exercise 1: Getting Started with PHP - javid</title>
 </head>
 <body>
     <h3>3.1 Write PHP code to output the following message:
